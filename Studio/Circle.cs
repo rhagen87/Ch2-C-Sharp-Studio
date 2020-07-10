0eb0ba9c054a9db190da2890d7eb298de0f2bc97
rhagen87 <reynold.hagen@gmail.com>
2020-07-10 13:28:17 -0500

    complete

---
+++ Studio/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Studio
+{
+    class Circle
+    {
+        public static double GetArea(double radius)
+        {
+            return System.Math.Pow(radius, 2) * System.Math.PI;
+        }
+
+        public static double GetCircum(double radius)
+        {
+            return 2 * System.Math.PI * radius;
+        }
+
+        public static double GetDiameter(double radius)
+        {
+            return 2 * radius;
+        }
+
+        public static double GetGallons(double radius, double mpg)
+        {
+            return Circle.GetCircum(radius) / mpg;
+        }
+    }
+}
---
+++ Studio/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Studio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the radius of the circle:");
+            string input = Console.ReadLine();
+            double radius = double.Parse(input);
+            Console.WriteLine("The area of the circle is: " + Circle.GetArea(radius));
+            Console.WriteLine("The circumference of the circle is: " + Circle.GetCircum(radius));
+            Console.WriteLine("The diameter of the circle is: " + Circle.GetDiameter(radius));
+            Console.WriteLine("Enter the MPG of the car:");
+            input = Console.ReadLine();
+            double mpg = double.Parse(input);
+            Console.WriteLine("It would require " + Circle.GetGallons(radius, mpg) + " gallons to drive around the circumference of the circle.");
+
+
+
+        }
+
+
+    }
+}
